c062d35685297173b8040ad7485a1a3ede1f6f29
Gui-Lima <ggfl@cin.ufpe.br>
2019-05-12 17:23:05 -0300

    Simple udp client and server

--- udp/src/Client.java
+++ udp/src/Client.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.*;
 
 public class Client {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPServer = InetAddress.getByName("localhost");
+        byte[] sendData;
+        sendData = ("Hello Friend").getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPServer, 5000);
+
+        long as = System.nanoTime();
+        clientSocket.send(sendPacket);
+        byte[] recieveData = new byte[1];
+        DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
+        clientSocket.receive(recievePacket);
+        System.out.println("RTT: " + (System.nanoTime() - as) / 1000 + "ms");
+        clientSocket.close();
+    }
 }
---
+++ udp/src/Server.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Server {
+    static int originPort = 5000;
+
+    public static void main(String[] args) throws IOException {
+        DatagramSocket serverSocket = new DatagramSocket(originPort);
+
+        byte[] recieveData = new byte[100];
+        byte[] senddata;
+        InetAddress clientIP;
+        int port;
+
+        while(true){
+            DatagramPacket recievePacket = new DatagramPacket(recieveData, recieveData.length);
+            System.out.println("Waiting in port " + originPort + "...");
+            serverSocket.receive(recievePacket);
+            String response = new String(recieveData);
+            System.out.println("Package Recieved!\n" + "Content: " + response);
+            clientIP = recievePacket.getAddress();
+            port = recievePacket.getPort();
+            senddata = ("2").getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(senddata, senddata.length, clientIP, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+
+
+}
